e7d8ee8336edf535c212dc5499f8554a3619dd61
Dayanara <56050496+BenitezDI@users.noreply.github.com>
2019-10-01 14:43:54 -0700

    Add files via upload

---
+++ Practice3.py
@@ -0,0 +1,15 @@
+## Dayanara Benitez 
+#September 26,2019
+#Period 6
+#Practice3.py
+
+week = input("What day of the week is it?")
+if week == "Monday":
+	print ("Its Monday, the weekend is over")
+
+elif week == "Friday":
+	print ("It's Friday, the weekend is close")
+elif week == "Saturday":
+	print ("It's the weekend, time to relax")
+else:
+	print("It's not the weekend yet")
---
+++ practice2.py
@@ -0,0 +1,17 @@
+# Dayanara Benitez 
+#September 25,2019
+#Period 6
+#practice2.py
+
+numberOne = input("Number1:")
+numberTwo = input("Number2:")
+
+
+print(numberOne + "-" + numberTwo + "=" + str(int(numberOne) - int(numberTwo)))
+
+
+print(numberOne + "+" + numberTwo + "=" + str(int(numberOne) + int(numberTwo)))
+
+print(numberOne + "*" + numberTwo + "=" + str(int(numberOne) * int(numberTwo)))
+
+print(numberOne + "/" + numberTwo + "=" + str(int(numberOne) / int(numberTwo)))
