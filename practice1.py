4c916a86775e4afb3fdcff60ce1992c3b5ec01e5
Dayanara <56050496+BenitezDI@users.noreply.github.com>
2019-10-01 14:43:34 -0700

    Add files via upload

---
+++ practice1.py
@@ -0,0 +1,18 @@
+#Dayanara Benitez
+#September 23,2019
+#Period 6
+#Practice1.py
+
+print ("4 * 8  = " + str (4 * 8))
+print ("12 / 2 = " + str (12 / 2))
+print ("-5 - 6 *3 = " + str (-5-6*3))
+
+age = input ("How old are you?: ")
+age = int (age) + 10 
+print("In ten years you will be " + str(age))
+print("Go Viper!\n" * 20 )
+myNum = 20 
+myNum = int(myNum) + 5
+myOtherNum = 10
+myNum = int (myNum) + myOtherNum 
+print ("My Num is " + str (myNum))
